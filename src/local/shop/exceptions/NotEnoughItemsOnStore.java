bd8eb3456947688a7c5ab9dfbaf4f409439b7d7e
Anton Boyarshinov <anton.unixxx@gmail.com>
2016-12-21 21:21:49 +0500

    fixed constructor signature

--- src/local/shop/exceptions/NotEnoughItemsOnStore.java
+++ src/local/shop/exceptions/NotEnoughItemsOnStore.java
@@ -1,20 +1,20 @@
 package local.shop.exceptions;
 
 
-import local.shop.model.deprecated.ShopItem;
+import local.shop.model.entity.ProductsEntity;
 
 public class NotEnoughItemsOnStore extends Throwable {
     private String err;
-    ShopItem item;
+    ProductsEntity item;
     public NotEnoughItemsOnStore(String err){
         this.err = err;
     }
-    public NotEnoughItemsOnStore(ShopItem item){
+    public NotEnoughItemsOnStore(ProductsEntity item){
         this.item = item;
     }
     public String getErrMsg(){
         if (err==null){
-            err="Недостаточно товаров на складе: "+item.getName()+" есть: "+item.getCountStored();
+            err="Недостаточно товаров на складе: "+item.getName()+" есть: "+item.getCount();
         }
         return err;
     }
