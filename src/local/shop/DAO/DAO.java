c7fad90d9145696d423a6673c2650baab670754f
Anton Boyarshinov <anton.unixxx@gmail.com>
2016-12-21 21:21:29 +0500

    added todo

--- src/local/shop/DAO/DAO.java
+++ src/local/shop/DAO/DAO.java
@@ -4,6 +4,10 @@ package local.shop.DAO;
 import java.util.List;
 
 public interface DAO<T> {
+    //TODO заменить параметр поиска по id на int
+    // либо заменить весь поиск на 1 метод,в котором будут поочередно проверяться все поля
+    // и поиск будет по недефолтному значению
+
     T selectById(T pattern);
     List<T> select();
     List<T> selectByName(T searchPattern);
